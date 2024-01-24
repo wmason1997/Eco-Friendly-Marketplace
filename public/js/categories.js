1b0fa19eae10fd55b95fd948670c19edf083523f
LE Palmer <lpalmer@ucsd.edu>
2024-01-23 21:44:49 -0800

    homepage categories display and handlebars

---
+++ public/js/categories.js
@@ -0,0 +1,35 @@
+const categories = [
+  {
+    name: 'Category 1',
+    imageUrl: 'http://',
+  },
+  {
+    name: 'Category 2',
+    imageUrl: 'http://',
+  },
+  {
+    name: 'Category 3',
+    imageUrl: 'http://',
+  },
+  {
+    name: 'Category 4',
+    imageUrl: 'http://',
+  },
+  {
+    name: 'Category 5',
+    imageUrl: 'http://',
+  },
+  {
+    name: 'Category 6',
+    imageUrl: 'http://',
+  },
+  {
+    name: 'Category 7',
+    imageUrl: 'http://',
+  },
+  {
+    name: 'Category 8',
+    imageUrl: 'http://',
+  },
+  // ... more categories if needed
+];
