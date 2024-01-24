ed639be6300fefee672d6bc8a1144c4dab287f07
LE Palmer <lpalmer@ucsd.edu>
2024-01-24 13:14:45 -0800

    created choseCategory function so when the user clicks a category image on the homepage they will be redirected to the subcategories page, reflected this in the handlebars file as well

--- public/js/categories.js
+++ public/js/categories.js
@@ -34,31 +34,8 @@ const categories = [
   // ... more categories if needed
 ];
 
+function choseCategory(itemID, subcategory,subcategoryImage) { //chooseCategory function will get called in handlebars html when user click category image. Will get take to the subcategories page of that category. 
 
-function choseCategory(itemID, quantity, price, imageURL) { // addToCart function will get called in handlebars html when the button is clicked
-
-  // item data to send to server 
-      const itemData = {  // fill in more properties
-          itemID: itemID,
-          quantity: quantity,
-          price: price,
-          imageURL: imageURL
-      };
-  
-      fetch('/cart/add/item', { // makes request to backend
-          method: 'POST',
-          body: JSON.stringify({itemData}), // fill out rest 
-          headers: { 'Content-Type': 'application/json' }
-      })
-   
-      .then(response => {response.json()
-      //window.reload to get backend to update the cart.
-      })
-      // .then(cartData => { // cartData is whatever gets sent back from the fetch request from backend
-      //     updateCartUI(cartData);
-      //     updateTotalPrice(cartData);
-      //     updateQuantity(cartData); // how would i do this with button dropdown? Comes from Bootstrap or wherever i get button icon from(michael)
-      // })
-      
-      .catch(error => console.error('Error:', error));
-  }
+    // Redirect to the subcategories page for the chosen category
+    window.location.href = `/items/${category}`;
+}
