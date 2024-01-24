a8520fbfcf966f1c9c27191a612b499b269789b2
LE Palmer <lpalmer@ucsd.edu>
2024-01-24 12:45:10 -0800

    starting function for when user chooses a Category

--- public/js/categories.js
+++ public/js/categories.js
@@ -33,3 +33,32 @@ const categories = [
   },
   // ... more categories if needed
 ];
+
+
+function choseCategory(itemID, quantity, price, imageURL) { // addToCart function will get called in handlebars html when the button is clicked
+
+  // item data to send to server 
+      const itemData = {  // fill in more properties
+          itemID: itemID,
+          quantity: quantity,
+          price: price,
+          imageURL: imageURL
+      };
+  
+      fetch('/cart/add/item', { // makes request to backend
+          method: 'POST',
+          body: JSON.stringify({itemData}), // fill out rest 
+          headers: { 'Content-Type': 'application/json' }
+      })
+   
+      .then(response => {response.json()
+      //window.reload to get backend to update the cart.
+      })
+      // .then(cartData => { // cartData is whatever gets sent back from the fetch request from backend
+      //     updateCartUI(cartData);
+      //     updateTotalPrice(cartData);
+      //     updateQuantity(cartData); // how would i do this with button dropdown? Comes from Bootstrap or wherever i get button icon from(michael)
+      // })
+      
+      .catch(error => console.error('Error:', error));
+  }
