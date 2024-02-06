67274998f3524c7dc181070c6cfa10319d0f04d7
LE Palmer <lpalmer@ucsd.edu>
2024-02-05 18:02:01 -0800

    resolved declaration logout error

---
+++ public/js/profile.js
@@ -0,0 +1,7 @@
+const showMyReviews = document.querySelector('#show-reviews-card-container');
+
+const reviewCards = document.querySelector('#review-cards-container');
+
+showMyReviews.addEventListener('click', () => {
+  console.log('clicked');
+});
